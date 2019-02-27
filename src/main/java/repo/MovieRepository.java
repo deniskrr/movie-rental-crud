2f7f46467850e8ae70ef624a943aaa7872d4bcb4
Deepst3r <craciunescu.denis12@gmail.com>
2019-02-27 20:59:21 +0200

    Make the repository class public

--- src/main/java/repo/MovieRepository.java
+++ src/main/java/repo/MovieRepository.java
@@ -3,7 +3,7 @@ package repo;
 import domain.Movie;
 import domain.Validator.MovieValidator;
 
-class MovieRepository extends InMemoryRepository<Movie> {
+public class MovieRepository extends InMemoryRepository<Movie> {
 
     public MovieRepository(MovieValidator validator) {
         super(validator);
