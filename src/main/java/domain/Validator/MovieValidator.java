9d2bb685c545f8b99c91830cc7969a7b89556425
Deepst3r <craciunescu.denis12@gmail.com>
2019-03-13 00:18:30 +0200

    Update movie validator

--- src/main/java/domain/Validator/MovieValidator.java
+++ src/main/java/domain/Validator/MovieValidator.java
@@ -6,13 +6,16 @@ public class MovieValidator implements Validator<Movie> {
     @Override
     public void validate(Movie entity) throws ValidatorException {
         if (entity.getTitle().trim().equals("")) {
-            throw new ValidatorException("Empty string");
+            throw new ValidatorException("Empty title");
         }
         if (entity.getRating() > 10 || entity.getRating() < 0) {
             throw new ValidatorException("Invalid rating");
         }
         if (entity.getYear() < 0 || entity.getYear() > 2019) {
             throw new ValidatorException("Invalid year");
+        }
+        if (entity.getGenre().trim().equals("")) {
+            throw new ValidatorException("Empty genre");
         }
     }
 }
