a2cb0a41a35a1fc517ed75ca97ef12d83bc3895b
Deepst3r <craciunescu.denis12@gmail.com>
2019-03-17 18:21:13 +0200

    Add XML ClientRepository

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,19 +8,24 @@ import domain.Validator.ClientValidator;
 import domain.Validator.MovieValidator;
 import domain.Validator.RentalValidator;
 import domain.Validator.Validator;
+import repo.IRepository;
+import repo.InMemoryRepository;
 import repo.file.ClientFileRepository;
 import repo.file.MovieFileRepository;
 import repo.file.RentalFileRepository;
+import repo.xml.ClientXmlRepository;
 import ui.Console;
+
+import java.util.UUID;
 
 public class Main {
     public static void main(String[] args) {
         Validator<Movie> movieValidator = new MovieValidator();
         Validator<Client> clientValidator = new ClientValidator();
         Validator<Rental> rentalValidator = new RentalValidator();
-        MovieFileRepository movieRepo = new MovieFileRepository(movieValidator,"data/movies.txt");
-        ClientFileRepository clientRepo = new ClientFileRepository(clientValidator,"data/clients.txt");
-        RentalFileRepository rentalRepo = new RentalFileRepository(rentalValidator,"data/rentals.txt");
+        IRepository<UUID, Movie> movieRepo = new MovieFileRepository(movieValidator,"data/movies.txt");
+        IRepository<UUID, Client> clientRepo = new ClientXmlRepository(clientValidator, "data/clients.xml");
+        IRepository<UUID, Rental> rentalRepo = new RentalFileRepository(rentalValidator,"data/rentals.txt");
         MovieController movieController = new MovieController(movieRepo);
         ClientController clientController = new ClientController(clientRepo);
         RentalController rentalController = new RentalController(rentalRepo);
