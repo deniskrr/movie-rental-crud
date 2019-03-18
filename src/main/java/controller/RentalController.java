f5c49d1d94042788024bca14acfd34ffe4b43dc4
Deepst3r <craciunescu.denis12@gmail.com>
2019-03-18 15:14:52 +0200

    Add most rented movie functionality

--- src/main/java/controller/RentalController.java
+++ src/main/java/controller/RentalController.java
@@ -6,6 +6,7 @@ import domain.Rental;
 import repo.IRepository;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -27,9 +28,13 @@ public class RentalController {
     }
 
     public List<Rental> getRentals() {
-        return  StreamSupport.stream(rentalRepository.findAll().spliterator(), false).collect(Collectors.toList());
+        return StreamSupport.stream(rentalRepository.findAll().spliterator(), false).collect(Collectors.toList());
     }
 
-
-
+    public UUID getMostRentedMovie() {
+        return getRentals().stream()
+                .collect(Collectors.groupingBy(Rental::getMovieID, Collectors.counting()))
+                .entrySet().stream().max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey).orElse(null);
+    }
 }
