f3c929ccf6a73fd99f1a27d4e170358829ec6e80
Deepst3r <craciunescu.denis12@gmail.com>
2019-03-20 19:52:27 +0200

    Modify movie database repo to not save in-memory

--- src/main/java/repo/db/MovieDatabaseRepository.java
+++ src/main/java/repo/db/MovieDatabaseRepository.java
@@ -2,67 +2,146 @@ package repo.db;
 
 import domain.Movie;
 import domain.Movie;
+import domain.Movie;
 import domain.Validator.Validator;
 import domain.Validator.ValidatorException;
+import repo.IRepository;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 
-public class MovieDatabaseRepository extends DatabaseRepository<Movie> {
+public class MovieDatabaseRepository implements IRepository<UUID, Movie> {
+    private static final String URL = "jdbc:postgresql://127.0.0.1:5433/Laboratory";
+    private static final String USERNAME = System.getProperty("username");
+    private static final String PASSWORD = System.getProperty("password");
+
+    private Validator<Movie> validator;
+
     public MovieDatabaseRepository(Validator<Movie> validator) {
-        super(validator);
-        this.loadData();
+        this.validator = validator;
     }
 
 
-    private void loadData() {
-        try (Connection con = DriverManager.getConnection(
-                "jdbc:postgresql://127.0.0.1:5433/Laboratory", "postgres",
-                "2233");
-             Statement statement = con.createStatement();
-             ResultSet rs = statement.executeQuery("SELECT id, title, rating, year, genre\n" +
-                     "\tFROM public.movies;")) {
-            while (rs.next()) {
-                UUID id = UUID.fromString(rs.getString(1));
-                String title = rs.getString(2);
-                double rating = Double.valueOf(rs.getString(3));
-                int year = Integer.valueOf(rs.getString(4));
-                String genre = rs.getString(5);
+    @Override
+    public Optional<Movie> findOne(UUID uuid) {
+        Movie movie = null;
+        String sql = "select * from Movies" +
+                " where id=?";
 
-                Movie movie = new Movie(title, rating, year, genre);
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME,
+                PASSWORD);
+             PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            if (resultSet.next()) {
+                UUID id = UUID.fromString(resultSet.getString("id"));
+                String title = resultSet.getString("title");
+                double rating = resultSet.getDouble("rating");
+                int year = resultSet.getInt("year");
+                String genre = resultSet.getString("genre");
+
+                movie = new Movie(title, rating, year, genre);
                 movie.setId(id);
-
-                super.save(movie);
             }
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return Optional.ofNullable(movie);
+    }
+
+    @Override
+    public Iterable<Movie> findAll() {
+        List<Movie> movies = new ArrayList<>();
+        String sql = "select * from Movies";
+
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME,
+                PASSWORD);
+             PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+                UUID id = UUID.fromString(resultSet.getString("id"));
+                String title = resultSet.getString("title");
+                double rating = resultSet.getDouble("rating");
+                int year = resultSet.getInt("year");
+                String genre = resultSet.getString("genre");
+
+
+                Movie movie = new Movie(title, rating, year, genre);
+                movie.setId(id);
+                movies.add(movie);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return movies;
     }
 
     @Override
     public Optional<Movie> save(Movie entity) throws ValidatorException {
-        Optional<Movie> optional = super.save(entity);
+        validator.validate(entity);
 
-        try (Connection con = DriverManager.getConnection(
-                "jdbc:postgresql://127.0.0.1:5433/Laboratory", "postgres",
-                "2233");
-             PreparedStatement statement = con.prepareStatement("INSERT INTO public.movies(\n" +
-                     "\tid, title, rating, year, genre)\n" +
-                     "\tVALUES (?, ?, ?, ?, ?);" +
-                     "")
-        ) {
+        String sql = "insert into movies(id,title, rating, year, genre) " +
+                "values (?,?,?,?,?)";
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME,
+                PASSWORD);
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
             statement.setString(1, entity.getId().toString());
             statement.setString(2, entity.getTitle());
             statement.setDouble(3, entity.getRating());
             statement.setInt(4, entity.getYear());
             statement.setString(5, entity.getGenre());
+
             statement.executeUpdate();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return Optional.empty();
         }
-        return optional;
+        return Optional.of(entity);
+    }
+
+    @Override
+    public Optional<Movie> delete(UUID uuid) {
+        String sql = "delete from movie " +
+                "where id=?";
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME,
+                PASSWORD);
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, uuid.toString());
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Movie> update(Movie entity) throws ValidatorException {
+        String sql = "update movie set title=?, rating=?, year=?, genre=? where id=?";
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME,
+                PASSWORD);
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, entity.getTitle());
+            statement.setDouble(2, entity.getRating());
+            statement.setInt(3, entity.getYear());
+            statement.setString(4, entity.getGenre());
+            statement.setString(5, entity.getId().toString());
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
+
+        return Optional.of(entity);
     }
 }
