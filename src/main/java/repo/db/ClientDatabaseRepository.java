7a0cfa3d00d71a74d65c1f3fc01670da563d113f
Deepst3r <craciunescu.denis12@gmail.com>
2019-03-20 19:45:35 +0200

    Modify client database repo to not save in-memory

--- src/main/java/repo/db/ClientDatabaseRepository.java
+++ src/main/java/repo/db/ClientDatabaseRepository.java
@@ -3,60 +3,138 @@ package repo.db;
 import domain.Client;
 import domain.Validator.Validator;
 import domain.Validator.ValidatorException;
+import repo.IRepository;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class ClientDatabaseRepository extends DatabaseRepository<Client> {
+public class ClientDatabaseRepository implements IRepository<UUID, Client> {
+    private static final String URL = "jdbc:postgresql://127.0.0.1:5433/Laboratory";
+    private static final String USERNAME = System.getProperty("username");
+    private static final String PASSWORD = System.getProperty("password");
+
+    private Validator<Client> validator;
+
     public ClientDatabaseRepository(Validator<Client> validator) {
-        super(validator);
-        this.loadData();
+        this.validator = validator;
     }
 
+    @Override
+    public Optional<Client> findOne(UUID uuid) {
+        Client client = null;
+        String sql = "select * from Clients" +
+                " where id=?";
 
-    private void loadData() {
-        try (Connection con = DriverManager.getConnection(
-                "jdbc:postgresql://127.0.0.1:5433/Laboratory", "postgres",
-                "2233");
-             Statement statement = con.createStatement();
-             ResultSet rs = statement.executeQuery("SELECT id, first_name, last_name, year\n" +
-                     "\tFROM public.clients;")) {
-            while (rs.next()) {
-                UUID id = UUID.fromString(rs.getString(1));
-                String firstName = rs.getString(2);
-                String lastName = rs.getString(3);
-                int year = Integer.valueOf(rs.getString(4));
-                Client client = new Client(firstName, lastName, year);
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME,
+                PASSWORD);
+             PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            if (resultSet.next()) {
+                UUID id = UUID.fromString(resultSet.getString("id"));
+                String firstName = resultSet.getString("first_name");
+                String lastName = resultSet.getString("last_name");
+                int year = resultSet.getInt("year");
+
+                client = new Client(firstName, lastName, year);
                 client.setId(id);
-                super.save(client);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return Optional.ofNullable(client);
+    }
+
+    @Override
+    public Iterable<Client> findAll() {
+        List<Client> clients = new ArrayList<>();
+        String sql = "select * from Clients";
+
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME,
+                PASSWORD);
+             PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+                UUID id = UUID.fromString(resultSet.getString("id"));
+                String firstName = resultSet.getString("first_name");
+                String lastName = resultSet.getString("last_name");
+                int year = resultSet.getInt("year");
+
+                Client client = new Client(firstName, lastName, year);
+                client.setId(id);
+                clients.add(client);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return clients;
     }
 
     @Override
     public Optional<Client> save(Client entity) throws ValidatorException {
-        Optional<Client> optional = super.save(entity);
+        validator.validate(entity);
 
-        try (Connection con = DriverManager.getConnection(
-                "jdbc:postgresql://127.0.0.1:5433/Laboratory", "postgres",
-                "2233");
-             PreparedStatement statement = con.prepareStatement("INSERT INTO public.clients(\n" +
-                     "\tid, first_name, last_name, year)\n" +
-                     "\tVALUES (?, ?, ?, ?);")
-        ) {
+        String sql = "insert into clients(id,first_name, last_name, year) " +
+                "values (?,?,?,?)";
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME,
+                PASSWORD);
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
             statement.setString(1, entity.getId().toString());
             statement.setString(2, entity.getFirstName());
             statement.setString(3, entity.getLastName());
             statement.setInt(4, entity.getYearOfBirth());
+
             statement.executeUpdate();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return Optional.empty();
         }
-        return optional;
+        return Optional.of(entity);
+    }
+
+    @Override
+    public Optional<Client> delete(UUID uuid) {
+        String sql = "delete from client " +
+                "where id=?";
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME,
+                PASSWORD);
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, uuid.toString());
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Client> update(Client entity) throws ValidatorException {
+        String sql = "update client set first_name=?, last_name=?, year=? where id=?";
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME,
+                PASSWORD);
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, entity.getFirstName());
+            statement.setString(2, entity.getLastName());
+            statement.setInt(3, entity.getYearOfBirth());
+            statement.setString(4, entity.getId().toString());
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
+
+        return Optional.of(entity);
     }
 }
