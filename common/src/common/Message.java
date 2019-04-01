c5fc8344395599b3cad13fdca5a967c916ff601b
Deepst3r <craciunescu.denis12@gmail.com>
2019-04-01 17:12:25 +0300

    Add common module

---
+++ common/src/common/Message.java
@@ -0,0 +1,48 @@
+package common;
+
+import java.io.*;
+
+public class Message {
+    public static final String ERROR = "error";
+
+    private static final String LINE_SEPARATOR = System.getProperty("line" +
+                                                                    ".separator");
+
+    private String header;
+    private String body;
+
+    public Message() {
+    }
+
+    public Message(String header, String body) {
+        this.header = header;
+        this.body = body;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+               "header='" + header + '\'' +
+               ", body='" + body + '\'' +
+               '}';
+    }
+
+    public void writeTo(OutputStream os) throws IOException {
+        os.write((header + LINE_SEPARATOR + body + LINE_SEPARATOR).getBytes());
+    }
+
+    public void readFrom(InputStream is) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+        header = br.readLine();
+        body = br.readLine();
+    }
+}
---
+++ common/src/common/service/MovieService.java
@@ -0,0 +1,8 @@
+package common.service;
+
+public interface MovieService {
+    final String SERVER_HOST = "localhost";
+    int SERVER_PORT = 1235;
+
+
+}
