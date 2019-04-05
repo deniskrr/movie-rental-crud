5b10861ae9ff7b5c056d590950f20b19c41040e9
Deepst3r <craciunescu.denis12@gmail.com>
2019-04-05 13:12:14 +0300

    Move domain to common module

--- common/src/domain/Validator/InvalidRatingException.java
+++ common/src/domain/Validator/InvalidRatingException.java
@@ -1,6 +1,4 @@
 package domain.Validator;
-import domain.Validator.ValidatorException;
-import domain.Movie;
 
 public class InvalidRatingException extends ValidatorException {
     public InvalidRatingException() { super("Invalid rating"); }
