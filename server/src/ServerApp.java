138addc56bd9565e51a90f73e6fcd775a83a9b01
Deepst3r <craciunescu.denis12@gmail.com>
2019-04-05 15:00:21 +0300

    Add server starting point

---
+++ server/src/ServerApp.java
@@ -0,0 +1,67 @@
+import domain.Client;
+import domain.Message;
+import domain.Movie;
+import domain.Rental;
+import domain.Validator.ClientValidator;
+import domain.Validator.MovieValidator;
+import domain.Validator.RentalValidator;
+import domain.Validator.Validator;
+import repo.Repository;
+import repo.db.MovieDatabaseRepository;
+import service.MovieService;
+import service.MovieServiceServerImplementation;
+import tcp.TcpServer;
+
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ServerApp {
+    public static void main(String[] args) {
+        ExecutorService executorService =
+                Executors.newFixedThreadPool(
+                        Runtime.getRuntime().availableProcessors());
+
+        TcpServer tcpServer = new TcpServer(executorService,
+                MovieService.SERVER_PORT);
+
+        Validator<Movie> movieValidator = new MovieValidator();
+        Validator<Client> clientValidator = new ClientValidator();
+        Validator<Rental> rentalValidator = new RentalValidator();
+        System.setProperty("username", "postgres");
+        System.setProperty("password", "2233");
+        Repository<UUID, Movie> movieRepo = new MovieDatabaseRepository(movieValidator);
+
+        MovieServiceServerImplementation movieService = new MovieServiceServerImplementation(executorService, movieRepo);
+
+        tcpServer.addHandler(MovieService.ADD_MOVIE, (request) -> {
+            String movieRequest = request.getBody();
+            String[] movieParams = movieRequest.split(",");
+            Movie movie = new Movie(movieParams[0],
+                    Double.valueOf(movieParams[1]),
+                    Integer.valueOf(movieParams[2]),
+                    movieParams[3]);
+            Future<String> result =
+                    movieService.addMovie(movie);
+            try {
+                return getMessage(Message.OK, result.get());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+                return getMessage(Message.ERROR, e.getMessage());
+            }
+
+        });
+
+        tcpServer.startServer();
+    }
+
+
+    private static Message getMessage(String header, String body) {
+        return Message.builder()
+                .header(header)
+                .body(body)
+                .build();
+    }
+}
