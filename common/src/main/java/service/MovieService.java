da2da4b0b0f05b188e3d3499bed7bdfbf3021bee
Deepst3r <craciunescu.denis12@gmail.com>
2019-04-15 10:56:02 +0300

    Change the remote interface methods return type

--- common/src/main/java/service/MovieService.java
+++ common/src/main/java/service/MovieService.java
@@ -5,14 +5,13 @@ import domain.Movie;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
 
 public interface MovieService extends Service {
 
-    CompletableFuture<Optional<Movie>> addMovie(String movieParams);
+    Optional<Movie> addMovie(String movieParams);
 
-    CompletableFuture<Optional<Boolean>> deleteMovie(UUID uid);
+    Optional<Boolean> deleteMovie(UUID uid);
 
-    CompletableFuture<List<Movie>> getMovies();
+    List<Movie> getMovies();
 
 }
