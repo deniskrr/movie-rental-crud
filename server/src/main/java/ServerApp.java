f666411d8ae64d10831875f79830144135f724d7
Deepst3r <craciunescu.denis12@gmail.com>
2019-04-15 11:17:22 +0300

    Set properties

--- server/src/main/java/ServerApp.java
+++ server/src/main/java/ServerApp.java
@@ -2,10 +2,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class ServerApp {
     public static void main(String[] args) {
+        System.setProperty("USERNAMESQL", "postgres");
+        System.setProperty("PASSWORDSQL", "2233");
+
+        System.out.println("Server started.");
 
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext("config");
-
 
     }
 }
