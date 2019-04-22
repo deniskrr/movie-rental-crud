2069625e563b1d2092e5ea4e7e8f3b035a8ffcdd
Deepst3r <craciunescu.denis12@gmail.com>
2019-04-22 16:11:37 +0300

    Add Logger

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,17 @@
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ui.Console;
 
 public class Main {
+    private static final Logger logger = LogManager.getLogger("mylogger");
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context=
                 new AnnotationConfigApplicationContext(
                         "config"
                 );
 
+        logger.info("Program started");
         Console console = context.getBean(Console.class);
         console.run();
 
