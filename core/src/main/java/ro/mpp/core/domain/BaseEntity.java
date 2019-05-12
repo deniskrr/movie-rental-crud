27d3e234d3eec2c4c511ef44c583219b6ac3ee5d
Deepst3r <craciunescu.denis12@gmail.com>
2019-05-12 13:20:48 +0300

    Update annotations

--- core/src/main/java/ro/mpp/core/domain/BaseEntity.java
+++ core/src/main/java/ro/mpp/core/domain/BaseEntity.java
@@ -5,36 +5,18 @@ import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+import lombok.*;
+
 /**
  * Superclass for every entity from the application.
  */
 @MappedSuperclass
+@NoArgsConstructor
+@Data
 public class BaseEntity<ID> implements Serializable {
     @Id
     @GeneratedValue
     private ID id;
 
-    /**
-     * Returns the ID of the entity.
-     * @return the ID
-     */
-    public ID getId() {
-        return id;
-    }
 
-    /**
-     * Sets the ID of the entity
-     * @param id of the entity
-     */
-    public void setId(ID id) {
-        this.id = id;
-    }
-
-
-    @Override
-    public String toString() {
-        return "BaseEntity{" +
-                "id=" + id +
-                '}';
-    }
 }
